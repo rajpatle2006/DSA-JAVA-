e914c05d0eebf56c4d9a5ef6e1d68225fc04836f
RAJ PATLE <rajpatle799@gmail.com>
2025-12-30 23:04:44 +0530

    Add files via upload

---
+++ Practice/Searchelement.java
@@ -0,0 +1,19 @@
+class D {
+    static class Node {
+        int data; Node next;
+        Node(int d) { data = d; }
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(10);
+        head.next = new Node(20);
+
+        int key = 20;
+        Node t = head;
+
+        while (t != null && t.data != key)
+            t = t.next;
+
+        System.out.print(t != null);
+    }
+}
