aa18de69bd717f4bdb4ffe11bda0a512bb7b98e3
RAJ PATLE <rajpatle799@gmail.com>
2026-01-05 20:37:39 +0530

    Add files via upload

---
+++ Practice/Merge2Array.java
@@ -0,0 +1,14 @@
+class M {
+    public static void main(String[] args) {
+        int[] a = {1,2};
+        int[] b = {3,4};
+        int[] c = new int[a.length + b.length];
+
+        int i=0;
+        for(int x : a) c[i++] = x;
+        for(int x : b) c[i++] = x;
+
+        for(int x : c)
+            System.out.print(x+" ");
+    }
+}
